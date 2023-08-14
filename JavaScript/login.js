f0e7cffa35d494c1f278d349c00a4603638855f3
kukue14 <kukue014@gmail.com>
2023-08-14 22:31:23 +0630

    14.8.2003 adding

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,8 +1,36 @@
 function login() {
-    var login = document.getElementById('rollnum').value;
-    if(login > 0 && login < 100) {
-        alert("You are Semester-3");
-        document.getElementById('toIndex').innerHTML;
+    // Get input values
+    var username = document.getElementById("username").value;
+    var rollno = document.getElementById('rollno').value;
+  
+    // Perform simple validation (you should do more robust validation on the server-side)
+    if (!username || !rollno) {
+      document.getElementById("loginStatus").innerHTML = "Please enter both username and roll no.";
+      return;
     }
-    else alert("You aren't Semester-3. This website is just for Semester-3.");
-}
+  
+    // Simulate a server-side login check (you should replace this with actual server-side validation)
+    // In this example, we assume the username is "admin" and the password is "password"
+    if (rollno > 21737 && rollno < 22581) {
+  
+     
+      window.location = "index.html"; // Redirecting to other page.
+      return false;
+      // document.getElementById("hello").isConnected = "Login successful! Welcome, " + username + ".";
+      //   var modal = document.getElementById('id01');
+  
+      //   // When the user clicks anywhere outside of the modal, close it
+      //   window.onclick = function (event) {
+      //     if (event.target == modal) {
+      //       modal.style.display = "none";
+      //     }
+      //   }
+  
+  
+  
+  
+  
+    } else {
+      document.getElementById("loginStatus").innerHTML = "Login failed. Your're not in Semester-3.";
+    }
+  }
