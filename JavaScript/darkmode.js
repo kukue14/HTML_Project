c103acb86ac46f9eabc3ecfb2f22e29a7a86451e
Wai Yan Htet <waiyanxtet04@gmail.com>
2023-08-25 20:26:24 +0630

    testing

--- JavaScript/darkmode.js
+++ JavaScript/darkmode.js
@@ -7,15 +7,15 @@ function changeImage() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     var image = document.getElementById('dark-light');
-    // if (image.src.match("turnOff")) {
-    //     image.src = "./images/turnOn.png";
-    // } else {
-    //     image.src = "./images/turnOff.png";
-    // }
+    if (image.src.match("turnOff")) {
+        image.src = "./images/turnOn.png";
+    } else {
+        image.src = "./images/turnOff.png";
+    }
 
 }
 
-function myFunction() {
-    var element = document.getElementById('dark-mode');
-    element.classList.toggle("dark-mode");
- }
+// function myFunction() {
+//     var element = document.getElementById('dark-mode');
+//     element.classList.toggle("dark-mode");
+//  }
