e65ba9452311ff0ca3f96eb775fe454c59a2e54c
Wai Yan Htet <waiyanxtet04@gmail.com>
2023-08-28 22:00:19 +0630

    adding something

--- JavaScript/darkmode.js
+++ JavaScript/darkmode.js
@@ -6,16 +6,37 @@
 function changeImage() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    var image = document.getElementById('dark-light');
-    if (image.src.match("turnOff")) {
-        image.src = "./images/turnOn.png";
-    } else {
-        image.src = "./images/turnOff.png";
+    var theme;
+    
+    // var image = document.getElementById('dark-light');
+
+
+    // if (image.src.match("turnOff")) {
+    //     image.src = "./images/turnOn.png";
+    //     theme = "DARK";
+    // } else {
+    //     image.src = "./images/turnOff.png";
+    //     theme = "LIGHT"
+    // }
+    if(element.classList.contains("dark-mode")){
+        console.log("Dark Mode");
+        theme = "DARK";
+    }else{
+        console.log("Light mode")
+        theme="LIGHT";
     }
 
-}
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
 
+}
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+console.log(GetTheme);
+
+if(GetTheme === "DARK"){
+    document.body.classList = "dark-mode";
+}
 // function myFunction() {
 //     var element = document.getElementById('dark-mode');
 //     element.classList.toggle("dark-mode");
 //  }
+
